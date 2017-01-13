5b7a1d46e5447e3be4a2458e15e0d9af688822ab
DARII Petru <pdarii@pentalog.fr>
2017-01-13 11:36:54 +0200

    multiple lists with same keys

--- src/app.js
+++ src/app.js
@@ -1,84 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-
-// This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <a href="#" onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </a>
-      </div>
-    );
-  }
-}
-
-function App() {
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
   return (
     <div>
-      <Clock />
+      {sidebar}
+      <hr />
+      {content}
     </div>
   );
 }
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
-class NumberList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {listItems: props.listItems};
-  }
-
-  render() {
-    const listItems = this.state.listItems.map((number) =>
-    <li key={number.toString()}>
-      {number} 
-    </li>
-    );
-
-    return <ul>{listItems}</ul>
-  }
-}
-const numbers = [1, 2, 3, 4, 5];
-
-
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 ReactDOM.render(
-  <NumberList listItems={numbers} />,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
