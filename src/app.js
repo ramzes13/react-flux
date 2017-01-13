c573f58caf8c45c30c3edc075172fc6998ac78db
DARII Petru <pdarii@pentalog.fr>
2017-01-13 11:16:00 +0200

    add binding function

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
+
+// This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -24,11 +27,19 @@ class Clock extends React.Component {
     });
   }
 
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <a href="#" onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </a>
       </div>
     );
   }
@@ -37,8 +48,6 @@ class Clock extends React.Component {
 function App() {
   return (
     <div>
-      <Clock />
-      <Clock />
       <Clock />
     </div>
   );
